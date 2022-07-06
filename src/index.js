a6e9dbe48719530da9d36d3ca1e2569162599942
DrobyshSV <austin@tut.by>
2022-07-06 23:53:53 +0300

    feat: add js code for task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,31 @@
 module.exports = function check(str, bracketsConfig) {
   // your solution
-}
+  let stack = [];
+  let arrayOfOpenedBrackets = bracketsConfig.map((t) => t[0]);
+  let arrayOfClosedBrackets = bracketsConfig.map((t) => t[1]);
+  for (let i = 0; i < str.length; i++) {
+    let currentBracket = str[i];
+    if (arrayOfOpenedBrackets.includes(currentBracket)) {
+      if (
+        stack[stack.length - 1] === currentBracket &&
+        arrayOfClosedBrackets.includes(currentBracket)
+      ) {
+        stack.pop();
+      } else {
+        stack.push(currentBracket);
+      }
+    } else {
+      if (stack.length === 0) {
+        return false;
+      }
+      let topElementOfStack = stack[stack.length - 1];
+      let index = arrayOfOpenedBrackets.indexOf(topElementOfStack);
+      if (str[i] === arrayOfClosedBrackets[index]) {
+        stack.pop();
+      } else {
+        return false;
+      }
+    }
+  }
+  return stack.length === 0;
+};
